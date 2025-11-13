e39560811aaa367f7cc7f15028bc6b43bf465c3a
Fannerys Guardia Cuesta <guardiacuestafannerys@gmail.com>
2025-11-13 12:25:35 -0500

    feat Se resuelve el ejercicio numero tres del taller

--- FannerysGuardia/script.js
+++ FannerysGuardia/script.js
@@ -1,6 +1,6 @@
-// //Ejercicio 1: Total de la compra
+// Ejercicio 1: Total de la compra
 
-// //Pedir los precios
+// Pedir los precios
 // let productoUno = Number (prompt ("Ingrese el precio del productoUno:"));
 // let productoDos = Number (prompt("Ingrese el precio del productoDos:"));
 // let productoTres = Number (prompt("Ingrese el precio del productoTres:"));
@@ -9,12 +9,22 @@
 // let totalCompra = (productoUno + productoDos + productoTres);
 // console.log("El total a pagar por los 3 productos es : " + totalCompra);
 
-//  Ejercicio 2 Banner
+//   Ejercicio 2 Banner
 
 //Pide el ancho y el alto en metros
-let anchoPared = Number(prompt("Ingrese el ancho de la pared:"));
-let altoPared = Number(prompt("Ingrese el largo de la pared"));
+// let anchoPared = Number(prompt("Ingrese el ancho de la pared:"));
+// let altoPared = Number(prompt("Ingrese el largo de la pared"));
 
 //Calcula su área
-let totalMaterial = (anchoPared * altoPared);
-console.log("El área del banner es: " + totalMaterial)
+// let totalMaterial = (anchoPared * altoPared);
+// console.log("El área del banner es: " + totalMaterial);
+
+//Ejercicio 3
+
+//Pedir el largo y ancho del potrero
+let largoPotrero = Number(prompt("Ingrese el largo del potrero"));
+let anchoPotrero = Number(prompt("Ingrese el ancho del potrero"));
+
+//Calcular perimetro
+let totalPerimetro = 2 * (largoPotrero + anchoPotrero);
+console.log("El perimetro del potrero es:" + t);
