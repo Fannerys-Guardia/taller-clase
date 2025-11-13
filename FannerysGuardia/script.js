e3eb66f117ee9918db61921f4eb2a4a829ecb970
Fannerys Guardia Cuesta <guardiacuestafannerys@gmail.com>
2025-11-13 12:13:48 -0500

    feat Se resuelve elsegundo punto del taller

--- FannerysGuardia/script.js
+++ FannerysGuardia/script.js
@@ -1,9 +1,20 @@
-console.log("Ejercicio 1: Total de la compra");
+// //Ejercicio 1: Total de la compra
 
-let productoUno = Number (prompt ("Ingrese el precio del productoUno:"));
-let productoDos = Number (prompt("Ingrese el precio del productoDos:"));
-let productoTres = Number (prompt("Ingrese el precio del productoTres:"));
+// //Pedir los precios
+// let productoUno = Number (prompt ("Ingrese el precio del productoUno:"));
+// let productoDos = Number (prompt("Ingrese el precio del productoDos:"));
+// let productoTres = Number (prompt("Ingrese el precio del productoTres:"));
 
-let totalCompra = (productoUno + productoDos + productoTres);
-console.log("El total a pagar por los 3 productos es : " + totalCompra);
+//Calcular elmonto
+// let totalCompra = (productoUno + productoDos + productoTres);
+// console.log("El total a pagar por los 3 productos es : " + totalCompra);
 
+//  Ejercicio 2 Banner
+
+//Pide el ancho y el alto en metros
+let anchoPared = Number(prompt("Ingrese el ancho de la pared:"));
+let altoPared = Number(prompt("Ingrese el largo de la pared"));
+
+//Calcula su área
+let totalMaterial = (anchoPared * altoPared);
+console.log("El área del banner es: " + totalMaterial)
