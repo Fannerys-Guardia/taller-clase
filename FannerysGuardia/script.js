056feb05ecf90f1a5eeadd6821503bfca81aef35
Fannerys Guardia Cuesta <guardiacuestafannerys@gmail.com>
2025-11-13 11:57:39 -0500

    feat se resuelve el promer punto del taller

---
+++ FannerysGuardia/script.js
@@ -0,0 +1,9 @@
+console.log("Ejercicio 1: Total de la compra");
+
+let productoUno = Number (prompt ("Ingrese el precio del productoUno:"));
+let productoDos = Number (prompt("Ingrese el precio del productoDos:"));
+let productoTres = Number (prompt("Ingrese el precio del productoTres:"));
+
+let totalCompra = (productoUno + productoDos + productoTres);
+console.log("El total a pagar por los 3 productos es : " + totalCompra);
+
